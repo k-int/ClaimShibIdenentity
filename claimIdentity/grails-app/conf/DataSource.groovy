7ce0c5882f2490fdddd690b69f5b300ef775f64b
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-12-23 09:44:04 +0000

    Added config for live database

--- claimIdentity/grails-app/conf/DataSource.groovy
+++ claimIdentity/grails-app/conf/DataSource.groovy
@@ -48,26 +48,23 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect=org.hibernate.dialect.MySQL5Dialect
+            username = "k-int"
+            password = "k-int"
+            url = "jdbc:mysql://localhost/ShibClaimId?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
+            pooled = true
+            // logSql = true
+            // formatSql = true
             properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+                maxActive = -1
+                minEvictableIdleTimeMillis=1800000
+                timeBetweenEvictionRunsMillis=1800000
+                numTestsPerEvictionRun=3
+                testOnBorrow=true
+                testWhileIdle=true
+                testOnReturn=true
+                validationQuery="select 1"
             }
         }
     }
