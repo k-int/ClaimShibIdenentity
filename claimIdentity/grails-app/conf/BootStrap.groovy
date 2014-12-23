254f5d4442c4aeac8abf271aea1c8d6f4f54c286
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-12-23 10:56:35 +0000

    added roles

--- claimIdentity/grails-app/conf/BootStrap.groovy
+++ claimIdentity/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
+import com.k_int.claimId.*
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+
+  def init = { servletContext ->
+    log.debug("Create roles if not present");
+    def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER', roleType:'global').save(failOnError: true)
+    def editorRole = Role.findByAuthority('ROLE_EDITOR') ?: new Role(authority: 'ROLE_EDITOR', roleType:'global').save(failOnError: true)
+    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN', roleType:'global').save(failOnError: true)
+    def kbplus_editor = Role.findByAuthority('KBPLUS_EDITOR') ?: new Role(authority: 'KBPLUS_EDITOR', roleType:'global').save(failOnError: true)
+    def apiRole = Role.findByAuthority('ROLE_API') ?: new Role(authority: 'ROLE_API', roleType:'global').save(failOnError: true)
+  }
+
+  def destroy = {
+  }
 }
